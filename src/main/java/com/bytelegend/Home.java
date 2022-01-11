04b23417130d3282c69c336a1aa7282b4712994f
gaozhidong <1011284295@qq.com>
2022-01-11 09:44:18 +0000

    Update at 2022-01-11T09:44:18.222213Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
 
-package com.bytelegend.pet.Cat;
-package com.bytelegend.pet.Dog;
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
 
 public class Home {
     public Cat cat;

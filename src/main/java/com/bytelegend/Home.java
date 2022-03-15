f9b24f339d06e7e32edddcf09a08220aa2b8f4f0
DUST2333 <1119736882@qq.com>
2022-03-15 08:17:46 +0000

    Update at 2022-03-15T08:17:46.825908Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,10 +1,11 @@
 package com.bytelegend;
-import com.bytelegend.Cat;
+
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
+
 public class Home {
     public static void main(String[] args) {
 
     }
     public Cat cat;
     public Dog dog;
-
-}

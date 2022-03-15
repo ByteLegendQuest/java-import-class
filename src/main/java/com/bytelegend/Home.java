39c2999b18069f189c17bee4c9cd816f8dd56f4a
DUST2333 <1119736882@qq.com>
2022-03-15 08:19:37 +0000

    Update at 2022-03-15T08:19:37.748014Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -4,8 +4,8 @@ import com.bytelegend.pet.Cat;
 import com.bytelegend.pet.Dog;
 
 public class Home {
-    public static void main(String[] args) {
 
-    }
     public Cat cat;
     public Dog dog;
+
+}

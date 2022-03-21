9dea8d5086f481116c0f88c977f51875e051b668
wl212 <2568247954@qq.com>
2022-03-21 01:59:45 +0000

    Update at 2022-03-21T01:59:45.398342Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,10 +1,8 @@
-import Cat;
-import Dog;
-package com.bytelegend;
 
+package com.bytelegend;
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
 public class Home {
-     public static void main(String[] args) {
     public Cat cat;
     public Dog dog;
-}
 }

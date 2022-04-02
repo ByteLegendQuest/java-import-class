6ea516277f6495df7b7c1beca95eb76590f26aa5
Xzar2 <colomban.vincent@gmail.com>
2022-04-02 17:45:32 +0000

    Update at 2022-04-02T17:45:32.204984Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,10 +1,6 @@
 package com.bytelegend;
-import public class Cat {
-
-}
-import public class Dog {
-    
-}
+import com.bytelegend.pet.Cat
+import com.bytelegend.pet.Dog
 public class Home {
     public Cat cat;
     public Dog dog;

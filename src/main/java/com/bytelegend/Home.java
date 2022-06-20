29d696a1834ef2cd96a0eebe8172e2e8e11756d9
laingzekungo <2698512450@qq.com>
2022-06-20 02:34:35 +0000

    Update at 2022-06-20T02:34:35.249467Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,6 +1,9 @@
 package com.bytelegend;
 
+import com.bytelegend.pet1.Cat;
+import com.bytelegend.pet2.*;
+
 public class Home {
-    public Cat cat;
-    public Dog dog;
+    Cat cat1=new Cat();
+    com.bytelegend.pet2.Cat cat2=new com.bytelegend.pet2.Cat();
 }

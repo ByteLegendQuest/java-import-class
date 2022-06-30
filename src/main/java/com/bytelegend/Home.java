0faf1b3759836a0e54da021bd9c283f153705175
Kaizokii <yelengyang@gmail.com>
2022-06-30 23:38:21 +0000

    Update at 2022-06-30T23:38:21.240151Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,6 +1,6 @@
 //package com.bytelegend;
-import pet.Cat;
-import pet.Dog;
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
 public class Home {
     public Cat cat;
     public Dog dog;

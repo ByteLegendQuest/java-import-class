d641cf102c400e2070f284ee562e25a9f6e950c2
Jonas3012 <jbgood1289@gmail.com>
2022-08-09 14:54:16 +0000

    Update at 2022-08-09T14:54:16.631402Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
 
-import com.bytelegend.pet.Cat.java;
-import com.bytelegend.pet.Dog.java;
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
 
 public class Home {
     public Cat cat;

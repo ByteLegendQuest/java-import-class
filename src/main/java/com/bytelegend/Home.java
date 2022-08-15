c9311cc9996aa526d05ffe414ea8f312c3e1efe7
NEWNEWWIN <1274697904@qq.com>
2022-08-15 05:56:29 +0000

    Update at 2022-08-15T05:56:29.353176Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,15 +1,6 @@
 package com.bytelegend;
 
-import org.junit.jupiter.api.Test;
-
-public class HomeTest {
-    @Test
-    public void homeHasCatAndDog() {
-        import Cat
-        import Dog
-        Home home;
-        home = new Home();
-        home.cat = new com.bytelegend.pet.Cat();
-        home.dog = new com.bytelegend.pet.Dog();
-    }
+public class Home {
+    public Cat cat;
+    public Dog dog;
 }

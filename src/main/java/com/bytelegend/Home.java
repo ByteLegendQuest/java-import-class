ebe3421731b72336050afe14b67b49b275f6a61f
NEWNEWWIN <1274697904@qq.com>
2022-08-16 10:01:03 +0000

    Update at 2022-08-16T10:01:03.703047Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,13 +1,8 @@
 package com.bytelegend;
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
 
-import bytelegend.pet.Dog();
-import bytelegend.pet.Cat();
-
-public class HomeTest {
-    @Test
-    public void homeHasCatAndDog() {
-        Home home = new Home();
-        home.cat = new com.bytelegend.pet.Cat();
-        home.dog = new com.bytelegend.pet.Dog();
-    }
+public class Home {
+    public Cat cat;
+    public Dog dog;
 }

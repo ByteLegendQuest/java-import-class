05ece5ea5399e6eeadf985629400dd751d026035
lulovelu <2972521663@qq.com>
2022-09-07 12:06:45 +0000

    Update at 2022-09-07T12:06:45.030491Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,7 @@
-package com.bytelegend.pet;
+package com.bytelegend;
 
-import Cat;
-import Dog;
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
 
 public class Home {
     public Cat cat;

73283639f31ab0591365676c29544bac3ca30c41
tiger0961 <29515547@qq.com>
2022-10-10 08:29:34 +0000

    Update at 2022-10-10T08:29:34.903547Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
 
-import com.bytelegend.cat;
-import com.bytelegend.dog;
+import com.bytelegend.pet.Cat;
+import com.bytelegend.pet.Dog;
 
 public class Home {
     public Cat cat;

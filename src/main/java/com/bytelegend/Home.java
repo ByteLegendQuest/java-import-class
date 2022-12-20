1f8dfdabbf776d6bf3f7d902f1d4dd61688ff7ae
Linx-APCI <apci-linx@outlook.com>
2022-12-20 07:42:54 +0000

    Update at 2022-12-20T07:42:54.258746Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,9 +1,8 @@
 package com.bytelegend;
+import static com.bytelegend.Home;
 
 public class Home {
     public Cat cat;
     public Dog dog;
-    import com.bytelegend.dog
-    import com.bytelegend.cat
 
 }
